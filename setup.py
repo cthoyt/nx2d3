f0052a9c7ea2da7d421930da0e2b38fcc1f9779e
Philip B <me@crabman.me>
2017-03-30 15:44:23 +0300

    Fix package not installing .py files
    Previously the package worked if installed with
    `pip install -e` but without `-e` flag it wouldn't
    work. Now it works.

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(name='nx2d3',
-      version='0.0.1',
+      version='0.0.2',
       description="A Python package for displaying NetworkX graphs inline in Jupyter notebooks",
       author='Charles Tapley Hoyt',
       author_email='cthoyt@gmail.com',
       url='https://github.com/cthoyt/nx2d3',
       license='MIT',
-      package_dir={'': 'src'},
-      install_requires=['networkx', 'IPython'],
-      zip_safe=False)
+      packages=['nx2d3'],
+      install_requires=['networkx', 'IPython'])
